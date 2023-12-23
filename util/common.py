b30491f3d5931b6d3c561754da63a311e9fdee2e
Pamasi <pamasideveloper@gmail.com>
2023-12-23 22:51:17 +0100

    update parser 4 qlora

--- util/common.py
+++ util/common.py
@@ -7,13 +7,16 @@ def get_args_parser():
     parser.add_argument('--path_val', default='data/processed/val.csv', type=str,
                         help="data path of val set")
     parser.add_argument('--max_len', default=120, type=int, help='max length of the tokenizer')
-    parser.add_argument('--batch', default=1, type=int, help='batch size')
-    parser.add_argument('--num_workers', default=1, type=int, help='number of workers')
+    parser.add_argument('--qlora', action='store_true', help='use qlora')
+    parser.add_argument('--batch', default=32, type=int, help='batch size')
+    parser.add_argument('--num_workers', default=2, type=int, help='number of workers')
     parser.add_argument('--score_level', default=5, type=int, help='level of scores')
     parser.add_argument('--device', default='cuda', type=str, help='device used to train')
     parser.add_argument('--lr', default=1e-5, type=float)
     parser.add_argument('--c_lr_min', default=1e-6, type=float)
     parser.add_argument('--c_lr_max', default=1e-4, type=float)
+    parser.add_argument('--margin', default=0.15, type=float)
     parser.add_argument('--n_epoch', default=30, type=int, help='number of epochs')
     parser.add_argument('--seed', default=23, type=int, help='seed')
+    parser.add_argument('--no_track', action='store_true', help='disable experiment tracking')
     return parser
